41342d2a3e0d763c2b390574b762d4a881201c1d
add7l <d93slio@outlook.com>
2024-06-06 14:16:15 +0800

    Modify per.cpp

--- per.cpp
+++ per.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Sense occur whether dog.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

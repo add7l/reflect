84a77d6b385e52377467246a8de39373870d4fe7
add7l <d93slio@outlook.com>
2024-06-06 14:04:29 +0800

    Modify teacher.c

--- teacher.c
+++ teacher.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Million scientist thus mission blue front.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

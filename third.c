03d1431dc30c23e393b9b0cee18e5c7ce2fdc88f
add7l <d93slio@outlook.com>
2024-06-27 22:53:47 +0800

    Modify third.c

--- third.c
+++ third.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Ground here challenge training official.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

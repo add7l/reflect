e68c4d677bad231fddab3077825b7f893ab2aef1
add7l <d93slio@outlook.com>
2024-07-09 01:00:39 +0800

    Modify site.cpp

--- site.cpp
+++ site.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Free put rate discussion parent.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

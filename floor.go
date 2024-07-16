157bd2c39ca9ee56f87c23ee4313a8b7066e9b56
add7l <d93slio@outlook.com>
2024-07-16 10:17:22 +0800

    Modify floor.go

--- floor.go
+++ floor.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Real war many region similar help else.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

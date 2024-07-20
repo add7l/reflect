7ec7856a9739e1dc2a6a4b5d89003e9982b0bd21
add7l <d93slio@outlook.com>
2024-07-20 13:53:08 +0800

    Modify stuff.cpp

--- stuff.cpp
+++ stuff.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Listen debate low.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

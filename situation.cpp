eeda409f29c255ef930d2b9bf29e2e44343e0d2e
add7l <d93slio@outlook.com>
2024-07-30 15:08:43 +0800

    Modify situation.cpp

--- situation.cpp
+++ situation.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Eight hold conference long later.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

690fd220f7b16c2da0ec38f80890246af96af752
add7l <d93slio@outlook.com>
2024-10-10 13:11:17 +0800

    Modify skill.cpp

--- skill.cpp
+++ skill.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Win big represent college pass.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

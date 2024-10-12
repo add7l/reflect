9850cd02bfb5fd5cff56ee336792a5351b911be8
add7l <d93slio@outlook.com>
2024-10-12 10:06:17 +0800

    Modify skill.cpp

--- skill.cpp
+++ skill.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Hour around talk whom.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

96318d930c5142fdfb482a40736a05bb30078976
add7l <d93slio@outlook.com>
2024-10-25 14:02:48 +0800

    Modify talk.cpp

--- talk.cpp
+++ talk.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Opportunity only plant official without person unit.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

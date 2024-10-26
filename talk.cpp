c192da6282b16ab468b33017ba16ba02a0f9bbe3
add7l <d93slio@outlook.com>
2024-10-26 13:43:34 +0800

    Modify talk.cpp

--- talk.cpp
+++ talk.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Even mind learn right realize couple everyone.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

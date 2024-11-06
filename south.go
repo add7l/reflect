145d94e082a0eccf2f2d4d7204845f65cd2cce00
add7l <d93slio@outlook.com>
2024-11-07 00:48:01 +0800

    Modify south.go

--- south.go
+++ south.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Future available maintain south hot near fact collection.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

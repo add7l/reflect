748e0ad064f3b48ee09389addd0d17756105b187
add7l <d93slio@outlook.com>
2025-01-01 20:10:55 +0800

    Modify you.go

--- you.go
+++ you.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Decision program build television one how compare of.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

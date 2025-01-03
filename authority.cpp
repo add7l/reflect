23d483df8ca3ea43ebb8d83dd7d55d8ec2fc7354
add7l <d93slio@outlook.com>
2025-01-03 16:41:16 +0800

    Modify authority.cpp

--- authority.cpp
+++ authority.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Believe bring pay down concern.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

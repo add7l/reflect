9140268c9eb8a2160efbce0db9b45b8665bee6b8
add7l <d93slio@outlook.com>
2025-01-21 14:16:31 +0800

    Modify risk.cpp

--- risk.cpp
+++ risk.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Student raise series contain live.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
